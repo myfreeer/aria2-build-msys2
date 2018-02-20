c5d42640ab98a3dec3f6ba37db3a0e7fffbb618d
myfreeer <myfreeer@users.noreply.github.com>
2018-02-20 21:03:08 +0800

    init-msys2.bat: fix catgets conflict

--- init-msys2.bat
+++ init-msys2.bat
@@ -24,18 +24,18 @@ busybox tar -Jxvf msys2-x86_64-latest.tar.xz
 %sh%  -lc "pacman-key --init && exit" 2>&1 | busybox tee -a init.log
 
 :Chi_Mirror
-for %%i in ( mirrorlist.mingw32 mirrorlist.mingw64 mirrorlist.msys) do if exist %%i copy /y %%i msys64\etc\pacman.d\
+for %%i in (mirrorlist.mingw32 mirrorlist.mingw64 mirrorlist.msys) do if exist %%i copy /y %%i msys64\etc\pacman.d\
 
 :Update
-%pacman% -Syyuu --needed --noconfirm 2>&1 | busybox tee -a update.log
-%pacman% -Suu --needed --noconfirm 2>&1 | busybox tee -a update.log
+%pacman% -Syyuu --needed --noconfirm --ask=20 2>&1 | busybox tee -a update.log
+%pacman% -Suu --needed --noconfirm --ask=20 2>&1 | busybox tee -a update.log
 
 :Install
 %pacman% -S --needed --noconfirm base-devel zlib-devel sqlite git unzip zip tar gmp gmp-devel libssh2 libssh2-devel openssl-devel 2>&1 | busybox tee -a install.log
 %pacman% -Sc --noconfirm 2>&1 | busybox tee -a install.log
 
 :Clone
-%sh%  -lc "if [[ -d ~/aria2 ]]; then cd aria2; git pull; else git clone https://gist.github.com/a780c730b7282e090f238e8286f815f3.git aria2; cd aria2; git pull; fi" 2>&1 | busybox tee -a clone.log
+%sh%  -lc "if [[ -d ~/aria2 ]]; then cd aria2; git pull; else git clone https://github.com/myfreeer/aria2-build-msys2.git aria2; cd aria2; git pull; fi" 2>&1 | busybox tee -a clone.log
 
 :Build
 %sh%  -lc "cd ~/aria2 && exec ./build-aria2.sh" 2>&1 | busybox tee -a build.log
