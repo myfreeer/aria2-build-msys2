add990233ac475f2441a26da371608bc78cc1c92
myfreeer <myfreeer@users.noreply.github.com>
2020-07-06 19:04:58 +0800

    build-aria2: skip some packages

--- build-aria2.sh
+++ build-aria2.sh
@@ -21,9 +21,6 @@ if [[ "${GIT_USER_EMAIL}" = "" ]]; then
 fi
 
 pacman -S --noconfirm --needed $MINGW_PACKAGE_PREFIX-gcc \
-    $MINGW_PACKAGE_PREFIX-expat \
-    $MINGW_PACKAGE_PREFIX-gmp \
-    $MINGW_PACKAGE_PREFIX-c-ares \
     $MINGW_PACKAGE_PREFIX-winpthreads
 
 PREFIX=/usr/local/$HOST
@@ -56,7 +53,7 @@ get_last_version() {
 # expat
 expat_ver="$(clean_html_index https://sourceforge.net/projects/expat/files/expat/ 'expat/[0-9]+\.[0-9]+\.[0-9]+')"
 expat_ver="$(get_last_version "${expat_ver}" expat '2\.\d+\.\d+')"
-expat_ver="${expat_ver:-2.2.8}"
+expat_ver="${expat_ver:-2.2.9}"
 wget -c --no-check-certificate "https://downloads.sourceforge.net/project/expat/expat/${expat_ver}/expat-${expat_ver}.tar.bz2"
 tar xf "expat-${expat_ver}.tar.bz2"
 cd "expat-${expat_ver}"
@@ -71,7 +68,7 @@ rm -rf "expat-${expat_ver}"
 
 # sqlite
 sqlite_ver=$(clean_html_index_sqlite "https://www.sqlite.org/download.html")
-[[ ! "$sqlite_ver" ]] && sqlite_ver="2019/sqlite-autoconf-3290000.tar.gz"
+[[ ! "$sqlite_ver" ]] && sqlite_ver="2020/sqlite-autoconf-3320300.tar.gz"
 sqlite_file=$(echo ${sqlite_ver} | grep -ioP "(sqlite-autoconf-\d+\.tar\.gz)")
 wget -c --no-check-certificate "https://www.sqlite.org/${sqlite_ver}"
 tar xf "${sqlite_file}"
@@ -91,7 +88,7 @@ rm -rf "${sqlite_name}"
 [[ ! "$cares_ver" ]] &&
     cares_ver="$(clean_html_index https://c-ares.haxx.se/)" &&
     cares_ver="$(get_last_version "$cares_ver" c-ares "1\.\d+\.\d")"
-cares_ver="${cares_ver:-1.15.0}"
+cares_ver="${cares_ver:-1.16.1}"
 echo "c-ares-${cares_ver}"
 wget -c --no-check-certificate "https://c-ares.haxx.se/download/c-ares-${cares_ver}.tar.gz"
 tar xf "c-ares-${cares_ver}.tar.gz"
